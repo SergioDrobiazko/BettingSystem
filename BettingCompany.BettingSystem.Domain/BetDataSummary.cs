3d5c37c3d33b122251a0b1d38eb56aeee7dfd134
Sergey Drobyazko <goodStudentSerg@gmail.com>
2025-06-03 17:57:48 +0200

    Add domain entities BetDataSummary, BetDataSummaryProvider

---
+++ BettingCompany.BettingSystem.Domain/BetDataSummary.cs
@@ -0,0 +1,13 @@
+﻿namespace BettingCompany.BettingSystem.Domain
+{
+    public class BetDataSummary
+    {
+        public int TotalProcessed { get; private set; }
+        public int TotalAmount { get; private set; }
+
+        public int TotalProfitOrLoss { get; private set; }
+
+        public int TopFiveWinners { get; private set; }
+        public int TopFiveLosers { get; private set; }
+    }
+}
