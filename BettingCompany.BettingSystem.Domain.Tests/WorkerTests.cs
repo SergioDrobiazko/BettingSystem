22b128d197c42648aa92c5c4d7bb57ff2cb4ac0f
Sergey Drobyazko <goodStudentSerg@gmail.com>
2025-06-04 12:26:20 +0200

    tests - Worker - Calculate Bet

--- BettingCompany.BettingSystem.Domain.Tests/WorkerTests.cs
+++ BettingCompany.BettingSystem.Domain.Tests/WorkerTests.cs
@@ -77,5 +77,24 @@ namespace BettingCompany.BettingSystem.Domain.Tests
 
             Assert.Equal(100, betOutcome.BetOutcome.Amount);
         }
+
+        [Fact]
+        public async Task CalculateBet_ClientVoid_CorrectOutcome()
+        {
+            var worker = new Worker();
+
+            var betArrivedFirst = new Bet(2, 1000, 5.5, "Sandra", "Usain Bolt 100m in London Olympics 2012", "", "Bolt breaks the WR", BetStatus.OPEN);
+            var betArrivedSecond = new Bet(2, 1000, 5.5, "Sandra", "Usain Bolt 100m in London Olympics 2012", "", "Bolt breaks the WR", BetStatus.VOID);
+
+            var betTransition = new BetTransition(betArrivedFirst, betArrivedSecond);
+
+            var betOutcomeTask = worker.CalculateBet(betTransition);
+
+            var betOutcome = await betOutcomeTask;
+
+            Assert.Equal(betOutcome.BetOutcome.Status, BetOutcomeStatus.Void);
+
+            Assert.Equal(1000, betOutcome.BetOutcome.Amount);
+        }
     }
 }
