d157ee8694cea423cf04bfffdcac89a1adcd15b9
Sergey Drobyazko <goodStudentSerg@gmail.com>
2025-06-04 11:36:22 +0200

    tests - Worker - Calculate Bet

--- BettingCompany.BettingSystem.Domain.Tests/WorkerTests.cs
+++ BettingCompany.BettingSystem.Domain.Tests/WorkerTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace BettingCompany.BettingSystem.Domain.Tests
@@ -6,9 +7,20 @@ namespace BettingCompany.BettingSystem.Domain.Tests
     public class WorkerTests
     {
         [Fact]
-        public void CalculateBet_IncorrectTransitionStatus_MarkedForReview()
+        public async Task CalculateBet_IncorrectTransitionStatus_MarkedForReview()
         {
+            var worker = new Worker();
 
+            var betArrivedFirst = new Bet(1, 100, 2.5, "Bob", "Lenox Luis vs Vitaly Klichko", "", "Klichko Wins", BetStatus.WINNER);
+            var betArrivedSecond = new Bet(1, 100, 2.5, "Bob", "Lenox Luis vs Vitaly Klichko", "", "Klichko Wins", BetStatus.OPEN);
+
+            var betTransition = new BetTransition(betArrivedFirst, betArrivedSecond);
+
+            var betOutcomeTask = worker.CalculateBet(betTransition);
+
+            var betOutcome = await betOutcomeTask;
+
+            Assert.True(betOutcome.IsMarkedForReview);
         }
     }
 }
---
+++ BettingCompany.BettingSystem.Domain/Worker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BettingCompany.BettingSystem.Domain
+{
+    public class Worker : IWorker
+    {
+        public Task<BetCalculated> CalculateBet(BetTransition betTransition)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
