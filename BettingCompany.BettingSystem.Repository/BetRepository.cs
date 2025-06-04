71310901a90c1c02ca7b91e22bac49080a5bd628
Sergey Drobyazko <goodStudentSerg@gmail.com>
2025-06-04 15:54:46 +0200

    save bets repository method placeholder

---
+++ BettingCompany.BettingSystem.Repository/BetRepository.cs
@@ -0,0 +1,14 @@
+﻿using BettingCompany.BettingSystem.Domain;
+using System;
+using System.Collections.Generic;
+
+namespace BettingCompany.BettingSystem.Repository
+{
+    public class BetRepository
+    {
+        public void Save(IEnumerable<Bet> bets)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
