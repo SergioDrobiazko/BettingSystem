305a31319618997f9878d5769fc6a9980c704d83
Sergey Drobyazko <goodStudentSerg@gmail.com>
2025-06-09 18:01:53 +0200

    more tests

---
+++ BettingCompany.BettingSystem.Domain.Tests/BetTransitionTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BettingCompany.BettingSystem.Domain.Tests
+{
+    public class BetTransitionTests
+    {
+        [Fact]
+        public void IsValidStatusTransition_FromOpenToWinner_IsValid()
+        {
+            var transition = new BetTransition(
+                new Bet(id: 1, amount: 420, odds: 4.08, client: "David", @event: "David vs Eve", market: "Correct Score", selection: "3:2", status: BetStatus.OPEN),
+                new Bet(id: 1, amount: 420, odds: 4.08, client: "David", @event: "David vs Eve", market: "Correct Score", selection: "3:2", status: BetStatus.WINNER));
+
+            Assert.True(transition.IsValidStatusTransition());
+        }
+
+        [Fact]
+        public void IsValidStatusTransition_FromOpenToLoser_IsValid()
+        {
+            var transition = new BetTransition(
+                new Bet(id: 1, amount: 420, odds: 4.08, client: "David", @event: "David vs Eve", market: "Correct Score", selection: "3:2", status: BetStatus.OPEN),
+                new Bet(id: 1, amount: 420, odds: 4.08, client: "David", @event: "David vs Eve", market: "Correct Score", selection: "3:2", status: BetStatus.LOSER));
+
+            Assert.True(transition.IsValidStatusTransition());
+        }
+
+        [Fact]
+        public void IsValidStatusTransition_FromOpenToVoid_IsValid()
+        {
+            var transition = new BetTransition(
+                new Bet(id: 1, amount: 420, odds: 4.08, client: "David", @event: "David vs Eve", market: "Correct Score", selection: "3:2", status: BetStatus.OPEN),
+                new Bet(id: 1, amount: 420, odds: 4.08, client: "David", @event: "David vs Eve", market: "Correct Score", selection: "3:2", status: BetStatus.VOID));
+
+            Assert.True(transition.IsValidStatusTransition());
+        }
+    }
+}
